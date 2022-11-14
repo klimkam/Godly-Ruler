32f827b4106fc210dcd511a2a37726caff5bc0d0
DenDenDor <dendor333@gmail.com>
2022-11-14 05:00:45 +0300

    Final Battle.

--- Assets/Scripts/Game/Trigger/DemonTrigger.cs
+++ Assets/Scripts/Game/Trigger/DemonTrigger.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AngleTrigger : Trigger
+public class DemonTrigger : Trigger
 {
-    [SerializeField] private Angel _angel;
+    [SerializeField] private Demon _demon;
     [SerializeField] private float _coolDown = 2;
     private bool _isReloaded = true;
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (_isReloaded)
         {
-            OnTrigger<Demon>(collision, e =>
+            OnTrigger<Angel>(collision, e =>
             {
-                e.Health.DecreaseHealth(_angel.Damage);
+                e.Health.DecreaseHealth(_demon.Damage);
                 StartCoroutine(CoolDown());
             });
         }
